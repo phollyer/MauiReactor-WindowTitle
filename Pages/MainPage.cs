d32f2ddc20ab283b0ee5472396de2a342bd11644
Paul Hollyer <paul@hollyer.me.uk>
2023-03-23 12:05:20 +0000

    Initial Commit

---
+++ Pages/MainPage.cs
@@ -0,0 +1,18 @@
+﻿using MauiReactor;
+
+namespace WindowTitle.Pages;
+
+class MainPage : Component
+{
+    public override VisualNode Render()
+    {
+        return new NavigationPage
+        {
+            new ContentPage
+            {
+            }
+        }
+        .WindowTitle("My Window");
+    }
+}
+
---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace WindowTitle;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
